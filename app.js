d2119e0e41a38b1f156f450d9a908b0626453d05
ces <franceschicecile@gmail.com>
2021-07-11 15:23:01 +0200

    user authentification

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
+// ajout express
 const express = require('express');
+// ajout bodyParser
+const bodyParser = require('body-parser');
+// On importe mongoose pour pouvoir utiliser la base de données
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb+srv://ces0715:ambush0715@cluster0.ikwgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// ajout headers pour CORS
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(bodyParser.json());
+app.use(express.json());
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+//const saucesRoutes = require('./routes/sauces');
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+//app.use('/api/sauces', saucesRoutes);
 
 
 module.exports = app;
