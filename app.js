876659046ff7df8c90195041c9a47dff095de406
ces <franceschicecile@gmail.com>
2021-07-19 16:50:44 +0200

    importer les routes

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 // ajout express
 const express = require('express');
-
 // Importer mongoose pour pouvoir utiliser la base de données
 const mongoose = require('mongoose');
+// importer le model de sauce
+//const Sauce = require('./models/Sauce');
 
-const sauceRoutes = require('./models/sauce');
+const saucesRoutes = require('./routes/Sauces');
 
-//declaration des routes
-//const saucesRoutes = require('./routes/sauces');
-// Importer la route dédiée aux utilisateurs
-const userRoutes = require('./routes/user');
-// création de l'application utilisant le framework express
-const app = express();
-
-
-
-mongoose.set('useCreateIndex', true);
 //connection à la base de données de MongoDB
 mongoose.connect('mongodb+srv://ces0715:ambush0715@cluster0.ikwgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// création de l'application utilisant le framework express
+const app = express();
 
 // ajout middleware headers pour CORS (debloquer systèmes de sécurité)
 app.use((req, res, next) => {
@@ -31,41 +25,21 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
-// Va servir les routes dédiées aux sauces
-
-app.post('/api/sauces', (req, res, next) => {
-  delete req.body._id;
-  const sauce = new Sauce({
-    ...req.body
-  });
-  sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-  
-
-
-app.use('/api/sauces', (req, res, next) => {
-  sauceRoutes.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauces/:id', (req, res, next) => {
-  sauceRoutes.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
-
 //transformation des données de la requete POST en JSON
 app.use(express.json());
 
+//declaration des routes
+const sauceRoutes = require('./models/Sauce');
+
+
+// Importer la route dédiée aux utilisateurs
+const userRoutes = require('./routes/user');
+
+mongoose.set('useCreateIndex', true);
+
 // Va servir les routes dédiées aux utilisateurs
 app.use('/api/auth', userRoutes);
-app.use('/api/sauces', sauceRoutes);
-
+app.use('/api/sauces', saucesRoutes);
 
 //Exportation de l'application express
 module.exports = app;
--- routes/sauces.js
+++ routes/sauces.js
@@ -1,5 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const Sauce = require('../models/Sauce');
+
+
+router.post('/', (req, res, next) => {
+    delete req.body._id;
+    const sauce = new Sauce({
+      ...req.body
+    });
+    sauce.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+      next();
+  });
+  
+  router.put('/:id', (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+      next();
+  });
+  
+  router.delete('/:id', (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+      next();
+  });
+  
+  // Va servir les routes dédiées aux sauces
+  router.get('/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(404).json({ error }));
+      next();
+  });
+  
+  // ou app.use
+  router.get('/', (req, res, next) => {
+    Sauce.find()
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(400).json({ error }));
+      next();
+  });
+
+
 
 const auth = require('../middleware/auth');
 const multer = require('..middleware/multer-config');
@@ -12,4 +57,6 @@ router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth, saucesCtrl.modifySauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 
+
 module.exports = router;
+
