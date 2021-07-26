a69232772c6b3127ea16aa84e8ae19d42f4688ff
ces <franceschicecile@gmail.com>
2021-07-26 16:39:43 +0200

    modif

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 //DECLARATION DES ROUTES
-// importer la route dédiée aux sauces
+//importer la route dédiée aux sauces
 const sauceRoutes = require('./routes/sauce');
 // Importer la route dédiée aux utilisateurs
 const userRoutes = require('./routes/user');
-
+// création de l'application utilisant le framework express
+const app = express();
 
 //connection à la base de données de MongoDB
 mongoose.connect('mongodb+srv://ces0715:ambush0715@cluster0.ikwgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -19,14 +20,12 @@ mongoose.connect('mongodb+srv://ces0715:ambush0715@cluster0.ikwgu.mongodb.net/my
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// création de l'application utilisant le framework express
-const app = express();
-
 // ajout middleware headers pour CORS (debloquer systèmes de sécurité)
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  
   next();
 });
 
@@ -40,7 +39,6 @@ app.use('/images',express.static(path.join(__dirname,'images')));
 
 // Va servir les routes dédiées aux utilisateurs
 app.use('/api/auth', userRoutes);
-
 // Va servir les routes dédiées aux sauces
 app.use('/api/sauces', sauceRoutes);
 
--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,74 +1,130 @@
 // recuperer le modele sauce
-const Sauce = require('../models/Sauce');
+const Sauce = require ('../models/Sauce');
 // recuperer modele file system pour les images
 const fs = require('fs');
 
-//route get
-exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-    next();
+
+//route post
+exports.createSauce = (req, res, next) => {
+  const sauce = new Sauce({
+   ...req.body
+  });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Sauce créée!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+  next();
 };
 
-exports.getAllSauces = (req, res, next) => {
+//route put
+exports.modifySauce = (req, res, next) => {
+  Sauce.updateOne({_id: req.params.id}, sauce).then(
+    () => {
+      res.status(201).json({
+        message: 'Sauce modifiée!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+  next();
+}
+
+//route delete
+exports.deleteSauce = (req, res, next) => {
+  Sauce.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Sauce supprimée!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+  next();
+}
+
+//route get
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id
+  }).then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+}
+
+exports.getAllSauces = (_req, res, next) => {
   Sauce.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
     next();
 };
 
-//route post
-exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.sauce);
-  delete sauceObject._id;
-  const sauce = new Sauce({
-    ...sauceObject,
-    imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  });
-  sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-    
-};
+// Création du like et dislike
+exports.likeSauce = (req, res, next) => {    
+  const like = req.body.like;
+  if(like === 1) { // Option like
+      Sauce.updateOne({_id: req.params.id}, { $inc: { likes: 1}, $push: { usersLiked: req.body.userId}, _id: req.params.id })
+      .then( () => res.status(200).json({ message: 'Vous aimez cette sauce !' }))
+      
+      .catch( error => res.status(400).json({ error}))
+  } else if(like === -1) { // Option dislike
+      Sauce.updateOne({_id: req.params.id}, { $inc: { dislikes: 1}, $push: { usersDisliked: req.body.userId}, _id: req.params.id })
+      .then( () => res.status(200).json({ message: 'Vous n\'aimez pas cette sauce !' }))
+      .catch( error => res.status(400).json({ error}))
 
-//route put
-exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file ?
-    {
-      ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-    next();
-};
-
-
-//route delete
-exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => {
-      const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlink(`images/${filename}`, () => {
-        Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-          .catch(error => res.status(400).json({ error }));
-      });
-    })
-    .catch(error => res.status(500).json({ error }));
-    next();
+  } else {    // Annuler like ou dislike
+      Sauce.findOne( {_id: req.params.id})
+      .then( sauce => {
+          if( sauce.usersLiked.indexOf(req.body.userId)!== -1){
+               Sauce.updateOne({_id: req.params.id}, { $inc: { likes: -1},$pull: { usersLiked: req.body.userId}, _id: req.params.id })
+              .then( () => res.status(200).json({ message: 'Vous n aimez plus cette sauce !' }))
+              .catch( error => res.status(400).json({ error}))
+              }
+          else if( sauce.usersDisliked.indexOf(req.body.userId)!== -1) {
+              Sauce.updateOne( {_id: req.params.id}, { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId}, _id: req.params.id})
+              .then( () => res.status(200).json({ message: 'Vous aimez cette sauce !' }))
+              .catch( error => res.status(400).json({ error}))
+              }           
+      })
+      .catch( error => res.status(400).json({ error}));             
+  next()  ;
 };
 
 
-//exports.deleteSauce = (req, res, next) => {
-  //Sauce.deleteOne({ _id: req.params.id })
-    //.then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    //.catch(error => res.status(400).json({ error }));
-    //next();
-//};
 
 
 
+
+
+
+
+
+
+};
  
