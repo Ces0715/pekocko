82a23379034915a9ac713324f9a6884d73f55c93
ces <franceschicecile@gmail.com>
2021-07-29 14:42:45 +0200

    modifier sauces

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const mongoose = require('mongoose');
 // donner acces au chemin (importer images)
 const path = require('path');
 
+//const cors =            require('cors');  
 
-//DECLARATION DES ROUTES
-//importer la route dédiée aux sauces
-const sauceRoutes = require('./routes/sauce');
-// Importer la route dédiée aux utilisateurs
-const userRoutes = require('./routes/user');
 // création de l'application utilisant le framework express
 const app = express();
 
+//app.use(cors({origin: 'http://localhost:4200'}));
 const Sauce = require('./models/Sauce');
 
 //connection à la base de données de MongoDB
@@ -42,21 +39,13 @@ mongoose.set('useCreateIndex', true);
 app.use('/images',express.static(path.join(__dirname,'images')));
 
 
-app.post('/api/sauces', (req, res, next) => {
-  delete req.body._id;
-  const sauce = new Sauce({
-    ...req.body
-  });
-  sauce.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
 
-app.use('/api/sauces', (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-});
+
+//DECLARATION DES ROUTES
+//importer la route dédiée aux sauces
+const sauceRoutes = require('./routes/sauce');
+// Importer la route dédiée aux utilisateurs
+const userRoutes = require('./routes/user');
 
 // Va servir les routes dédiées aux utilisateurs
 app.use('/api/auth', userRoutes);
